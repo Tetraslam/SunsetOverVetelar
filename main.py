848e371f5c27e0a52c8da65fd6288ef57be4e5db
Tetraslam <bhowmickshresht@gmail.com>
2023-02-24 13:34:45 +0530

    changed update rate

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import pygame
 
 pygame.init()
-win = pygame.display.set_mode((1600, 1000))
+win = pygame.display.set_mode((1600, 900))
 pygame.display.set_caption("Sunset Over Veletar")
 
 x = 50
 y = 50
+vel = 10
+
 width = 400
 height = 600
-vel = 5
 
 run = True
 
 while run:
-    pygame.time.delay(100)
+    pygame.time.delay(10)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -37,4 +38,5 @@ while run:
     pygame.draw.rect(win, (255,0,0), (x, y, width, height))   
     pygame.display.update() 
     
+
 pygame.quit()
